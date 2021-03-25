8ba6985b907ab9567dfc7ce6aac7a78cde327fa1
unknown <weeslleey354@gmail.com>
2021-03-25 13:37:25 -0300

    import json webtoken on test

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -13,7 +13,22 @@ describe("Authentication", () => {
     const user = await User.create({
       name: "Weslley",
       email: "weslley@gmail.com",
-      password_hash: "123456",
+      password: "123456",
+    });
+
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "123",
+    });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not authenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "Weslley",
+      email: "weslley@gmail.com",
+      password: "123456",
     });
 
     const response = await request(app).post("/sessions").send({
@@ -21,6 +36,8 @@ describe("Authentication", () => {
       password: "123456",
     });
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(401);
   });
+
+  it("should return jwt token when authenticated", async () => {});
 });
