93e01344f3d78abbca8ad6ec1ee52bd74daa3531
unknown <weeslleey354@gmail.com>
2021-03-25 11:20:20 -0300

    test authenticate

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
+
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should authentication with valid credentials", async () => {
+    const user = await User.create({
+      name: "Weslley",
+      email: "weslley@gmail.com",
+      password_hash: "123456",
+    });
+
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "123456",
+    });
+
+    expect(response.status).toBe(200);
+  });
+});
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,7 @@
 const routes = requires("express");
 
+const SessionController = require("./app/controllers/SessionController");
+
+routes.post("/sessions", SessionController.store);
+
 module.exports = routes;
